e32d3a6239f1a7a6ce4038de58986aceca51f178
jorgeabrahan <jorge24abrahan@gmail.com>
2023-05-21 02:57:12 -0600

    Add sql server and scoped services to program

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using vacaciones_empleado;
+using vacaciones_empleado.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSqlServer<VacacionesEmpleadoContext>("Data Source=localhost; Initial Catalog=vacationsDB;user id=sa;password=Programaci0n$;Encrypt=False");
+builder.Services.AddScoped<ICargoService, CargoService>();
+builder.Services.AddScoped<ICodigoTrabajoService, CodigoTrabajoService>();
+builder.Services.AddScoped<IEmpleadoService, EmpleadoService>();
+builder.Services.AddScoped<IVacacionesService, VacacionesService>();
 
 var app = builder.Build();
 
